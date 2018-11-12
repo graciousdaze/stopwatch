0f00f7b0794cb95a44278ccc5a11615e1ea6050f
Grace <graciousdaze@gmail.com>
2018-11-11 22:38:19 -0600

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new StopWatch();
+
+            Console.WriteLine("Enter (s) to start the stop watch or (e) to exit: ");
+            var input = Console.ReadLine();
+            bool notStarted = true;
+
+            while (notStarted)
+            {
+                if (input.ToLower() == "s")
+                {
+                    watch.Start();
+                    break;
+                }
+                else if (input.ToLower() == "e")
+                {
+                    Console.WriteLine("Ending the program, bye now. (Press enter to close window)");
+                    break;
+                }
+
+                Console.WriteLine("Invalid entry, try again. Press (s) to start or (e) to exit");
+                input = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private TimeSpan _startTime;
+        private TimeSpan _duration;
+        private bool _isRunning = false;
+
+        public void Start()
+        {
+            if (this._isRunning)
+                throw new InvalidOperationException("Stopwatch is already running");
+
+            this._startTime = DateTime.Now.TimeOfDay;
+            this._isRunning = true;
+
+            Console.WriteLine("Stop watch has begun, enter (p) to pause the watch or (e) to exit");
+            var input = Console.ReadLine();
+
+            while (this._isRunning)
+            {
+                if (input == "e")
+                {
+                    Stop();
+                    break;
+                }
+                else if (input == "p")
+                {
+                    Pause();
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please try again and enter (p) to pause or (e) to exit.");
+                input = Console.ReadLine();
+            }
+
+
+        }
+
+        public void Pause()
+        {
+            this._isRunning = false;
+            this._duration = this._duration + (DateTime.Now.TimeOfDay - this._startTime);
+
+            Console.WriteLine("Stop watch paused. Current Tracked Time: {0:D2}:{1:D2} \nEnter (c) to continue or (e) to exit", this._duration.Minutes, this._duration.Seconds);
+            var input = Console.ReadLine();
+
+            while (!this._isRunning)
+            {
+                if (input == "c")
+                {
+                    Start();
+                    break;
+                }
+                else if (input == "e")
+                {
+                    Stop();
+                    break;
+                }
+
+                Console.WriteLine("Invalid input. Please try again and press (c) to continue or (e) to exit.");
+                input = Console.ReadLine();
+            }
+        }
+
+        public void Stop()
+        {
+            this._isRunning = false;
+            var time = this._duration == TimeSpan.Zero ? DateTime.Now.TimeOfDay - this._startTime : this._duration;
+            Console.WriteLine("Ending the stopwatch, finished time is: {0:D2}:{1:D2}", time.Minutes, time.Seconds);
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Console.WriteLine("Would you like to reset the stopwatch? (Y/N)");
+            var input = Console.ReadLine();
+            switch (input.ToLower())
+            {
+                case "y":
+                    Start();
+                    break;
+                case "n":
+                    Console.WriteLine("Closing the program, bye now. (Press enter to close window)");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, try again");
+                    Reset();
+                    break;
+            }
+        }
+    }
+}
